2a8c4afc238693981c5dff4b73322ebcfb2cbff4
Alexandros Avgoustis <alexandavg@192.168.2.20>
2026-01-04 20:19:32 +0200

    Add complete bloxy type declarations with all methods

--- src/types/bloxy.d.ts
+++ src/types/bloxy.d.ts
@@ -1,9 +1,12 @@
 declare module 'bloxy' {
   export class Client {
     constructor(options?: any);
-    login(cookie: string): Promise<any>;
+    user: any;
+    apis: any;
+    login(cookie?: string): Promise<any>;
     getUser(userId: number): Promise<any>;
     getGroup(groupId: number): Promise<any>;
+    getUsersByUsernames(usernames: string[]): Promise<any>;
   }
 }
 
@@ -12,11 +15,15 @@ declare module 'bloxy/dist/structures' {
     id: number;
     name: string;
     username?: string;
+    getJoinRequestInGroup(groupId: number): Promise<any>;
+    getPrimaryGroup(): Promise<any>;
   }
 
   export class PartialUser {
     id?: number;
     name?: string;
+    getJoinRequestInGroup?(groupId: number): Promise<any>;
+    getPrimaryGroup?(): Promise<any>;
   }
 
   export class GroupMember {
@@ -30,12 +37,29 @@ declare module 'bloxy/dist/structures' {
     };
   }
 
+  export class GroupRole {
+    id: number;
+    name: string;
+    rank: number;
+  }
+
+  export class GroupJoinRequest {
+    requester: {
+      userId: number;
+      username: string;
+    };
+  }
+
   export class Group {
     id: number;
     name: string;
     updateMember(userId: number, roleId: number): Promise<any>;
-    getJoinRequests(): Promise<any>;
+    getJoinRequests(options?: any): Promise<any>;
     acceptJoinRequest(userId: number): Promise<any>;
     declineJoinRequest(userId: number): Promise<any>;
+    getMember(userId: number): Promise<any>;
+    getRoles(): Promise<any>;
+    kickMember(userId: number): Promise<any>;
+    updateShout(message: string): Promise<any>;
   }
 }
